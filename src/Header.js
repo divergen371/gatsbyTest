f5ea69f979b883cffcb4cd0551cbb7ba03a85441
divergen371 <divergen@protonmail.com>
2021-07-13 13:24:54 +0900

    update

---
+++ src/Header.js
@@ -0,0 +1,30 @@
+import * as styles from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <header className={styles.container}>
+      <h1 className={styles.siteName}>
+        <a href="/">My Site</a>
+      </h1>
+      <ul className={styles.nav}>
+        <li className={styles.navItem}>
+          <a href="/">About</a>
+        </li>
+        <li className={styles.navItem}>
+          <a href="/">Works</a>
+        </li>
+      </ul>
+      <div className={styles.social}>
+        <a
+          href="https://github.com/divergen371"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img width={32} height={32} alt="Github" src="/public/GitHub.png" />{" "}
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
